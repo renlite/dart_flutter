9219bf610583aa0c5f14f1f4c0255c3363756e4f
renlite <jsmi@live.at>
2021-05-06 09:30:14 +0200

    Init_01

---
+++ renlite_sample_page/lib/master.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart' show Colors;
+import 'package:renlite/foundation.dart';
+import 'package:renlite/rendering.dart';
+import 'app_service.dart';
+
+class MasterService extends RenderProxyBox with RenliteKeyMixin {
+  String name = "Goto Detail";
+  late AppService app;
+
+  MasterService({String key = "One", required RenliteService app}) {
+    this.key = key;
+    this.app = app as AppService;
+    this.child = this.build(app);
+  }
+
+  RenderBox build(RenliteService app) {
+    return RenliteTransform(
+        key: "Transform",
+        transform: Matrix4.identity(),
+        child: RenderPositionedBox(
+          alignment: Alignment.center,
+          child: RenliteSimpleButton(
+              key: "MasterButton",
+              onTab: app.onTab,
+              decoration: BoxDecoration(color: Colors.blue),
+              child: RenderSizedOverflowBox(
+                  requestedSize: Size(400.0, 400.0),
+                  child: RenderParagraph(
+                      TextSpan(
+                        text: this.name,
+                        // appService?.greeting,
+                        // Casting (this.renliteAppService as AppService).greeting
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                      textDirection: TextDirection.ltr))),
+        ));
+  }
+
+  void onTab(PointerDownEvent event, RenliteKeyMixin renderObject) {
+    this.app.tree?.renderView;
+  }
+}
